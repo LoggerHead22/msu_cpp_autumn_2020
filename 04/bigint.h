ab928c571019b031f83a0d20b8d84f84f8b24a91
LoggerHead22 <LoggerHead22@yandex.ru>
2020-11-06 05:58:56 +0300

    header fixed

--- 04/bigint.h
+++ 04/bigint.h
@@ -17,67 +17,66 @@ private:
 	int *numBlocks = nullptr;//массив блоков
 	size_t blockCount = 0, numberSize = 0;//Число блоков, длина числа
 	bool isNegative = false; //Флаг орицательности
-	
+
 public:
     //Конструкторы
     BigInt(const string& number);
-	
 	BigInt(const BigInt&);
 	BigInt(int);
-	
 	BigInt(BigInt&& moved);
+
 	//Деструктор
 	~BigInt();
-		
+
     //Операторы
 	//копирующие
     BigInt& operator=(const BigInt& other);
 	BigInt& operator=(int other);
 	//перемещающие
     BigInt& operator=(BigInt&& moved);
-	
+
 	//Сложение, вычитание
 	BigInt operator+(const BigInt& other) const;
     BigInt operator+(int other) const;
-    
+
 	BigInt operator-(const BigInt& other) const;
     BigInt operator-(int other) const;
-	
+
     BigInt& operator+=(const BigInt& other);
     BigInt& operator+=(int other);
-	
+
 	BigInt& operator-=(const BigInt& other);
     BigInt& operator-=(int other);
 	//Умножение
 	BigInt operator*(const BigInt& other) const;
     BigInt operator*(int other) const;
-	
+
 	BigInt& operator*=(const BigInt& other);
     BigInt& operator*=(int other);
-	
+
 	//Сравнение
     bool operator<(const BigInt& other) const;
     bool operator<=(const BigInt& other) const;
-	
+
 	bool operator>(const BigInt& other) const;
     bool operator>=(const BigInt& other) const;
-	
+
     bool operator==(const BigInt& other) const;
     bool operator!=(const BigInt& other) const;
 
     bool operator<(int other) const;
     bool operator<=(int other) const;
-	
+
 	bool operator>(int other) const;
     bool operator>=(int other) const;
-		
+
     bool operator==(int other) const;
     bool operator!=(int other) const;
 	//Унарный минус
 	BigInt operator-() const;
 	//Вывод
 	friend ostream& operator<<(ostream& stream, const BigInt& number);
-	
+
 private:
 	bool _isDigit(const string &number) const;
 	bool _absLessEq(const BigInt& other) const;
