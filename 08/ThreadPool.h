75e955631e4df6785c39396cb8a6f863cf2f03bc
Your Name <you@example.com>
2020-12-03 06:47:49 +0300

    hw8

--- 08/ThreadPool.h
+++ 08/ThreadPool.h
@@ -4,6 +4,7 @@
 #include <queue>
 #include <mutex>
 #include <future>
+#include <functional>
 using namespace std;
 
 
@@ -11,30 +12,75 @@ using namespace std;
 
 class ThreadPool
 {
+private:
+    size_t poolSize_ = thread::hardware_concurrency();
+    bool terminate = false;
+    vector<thread> threads_;
+    queue<packaged_task<void()>> tasks_;
+    mutex queue_mutex;
+    condition_variable new_task;
+
 public:
 
-	
     explicit ThreadPool(size_t poolSize)
-	:poolSize_(poolSize)
-	{
-		for(size_t i = 0; i < poolSize_; i++)
-		{
-			threads_.push_back(thread([=] {exec_tasks();}));
-		}	
-			
-	}
-	void exec_tasks(){
-		
-	}
+    :poolSize_(poolSize)
+    {
+        for(size_t i = 0; i < poolSize_; i++)
+        {
+            threads_.push_back(thread([this](){ exec_tasks(); }));
+        }
+    }
 
-     // pass arguments by value
     template <class Func, class... Args>
-    auto exec(Func func, Args... args) -> std::future<decltype(func(args...))>;
+    auto exec(Func func, Args... args) -> std::future<decltype(func(args...))>{
 
+        std::packaged_task<decltype(func(args...))()> task(bind(func, forward<Args>(args)...));
 
-	
+        auto result = task.get_future();
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+
+            tasks_.emplace(std::move(task));
+        }
+        new_task.notify_one(); //сообщить потокам о новой задаче
+        return result;
+    }
+
+    ~ThreadPool(){
+        stop();
+    }
+
+    void stop(){
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            terminate = true;
+        }
+
+        new_task.notify_all();
+        for(auto& t: threads_){
+            t.join();
+        }
+    }
+
 private:
-	size_t poolSize_ = thread::hardware_concurrency();
-	vector<thread> threads_;
-	
+
+    void exec_tasks(){
+
+        while(true){
+            std::packaged_task<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                if (tasks_.empty()){//если очередь пустая
+                    new_task.wait(lock, [&]{ return terminate || !tasks_.empty(); }); //ждем сигнала, если очередь не пуста или terminate, то идем дальше
+                }
+                if(terminate){
+                    return;
+                }
+                task = std::move(tasks_.front());
+                tasks_.pop();
+            }
+            task();
+        }
+    }
+
 };
--- 08/test.cpp
+++ 08/test.cpp
@@ -1,19 +1,74 @@
 #include <cassert>
+#include <cmath>
 #include "ThreadPool.h"
 
-struct A {};
+struct A {
+    int n1 = 1;
+    int n2 = 21;
+};
 
-void foo(const A&) {}
+int foo(const A& a) {
+   return a.n1 + a.n2;
+}
+
+string concat(string s1, string s2){
+    return s1 + s2;
+}
+
+void sleeper(int time){
+    this_thread::sleep_for(chrono::seconds(time));
+    cout<<" Im go to sleep!"<<endl;
+}
+
+float power(float x, float y){
+    return pow(x, y);
+}
+
+void TestCase1(){
+    ThreadPool pool(8);
+
+    auto task1 = pool.exec(foo, A());
+    assert(task1.get() == 22);
+
+    auto task2 = pool.exec(concat, "Techno", "Sphere");
+    assert(task2.get() == "TechnoSphere");
+}
+
+void TestCase2(){
+    ThreadPool pool(8);
+
+    vector<future<float>> tasks;
+    for(size_t i = 1; i < 20; i++){
+        tasks.push_back(pool.exec(power, i, 2));
+    }
+
+    for(size_t i = 1; i < 20; i++){
+        assert(tasks[i - 1].get() == pow(i, 2));
+    }
+
+}
+
+void TestCase3(){
+    //Хотим показать, что вывод первой функции будет после
+    //вызовов возведения в степень
+    ThreadPool pool(8);
+    cout<<"Test Case 3"<<endl;
+    auto task = pool.exec(sleeper, 2);
+
+    vector<future<float>> tasks;
+    for(size_t i = 1; i < 20; i++){
+        tasks.push_back(pool.exec(power, i, 2));
+    }
+    cout<<" Power is ready!"<<endl;
+}
+
+
 
 int main(){
-    ThreadPool pool(8);
-    
-    //auto task1 = pool.exec(foo, A());
-    //task1.get();
-    //
-    //auto task2 = pool.exec([]() { return 1; });
-    //task2.get();
-    //
-    
+    TestCase1();
+    TestCase2();
+    TestCase3();
+
+    cout<<"Success!"<<endl;
     return 0;
 }
